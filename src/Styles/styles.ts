43e448b731df632838f05ff615911d0589f8a51c
SreenivasaReddy <sreenivas.ecm@gmail.com>
2021-05-16 17:51:15 -0500

    adding styes external file

--- src/Styles/styles.ts
+++ src/Styles/styles.ts
@@ -1,22 +1,4 @@
 export const styles={
-    avatar: { 
-        height: '100px', 
-        width: '100px' 
-    },
-    user:{
-        marginTop:10
-    },
-    pos: {
-        paddingRight:20,
-        
-    },
-    reposTitle:{
-        marginTop:30
-    },
-    repos:{
-        marginLeft:100,
-        marginBottom:50,
-        marginRight:50
-    },
+    
 
 }
